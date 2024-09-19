7ae445c3c751098e90e7cc2f512bd8d42ad7efa3
Jorge Vega <j.vega@mafasa.org>
2024-09-19 09:10:42 -0700

    Se agrega el campo de imagen al proyecto

--- models/Producto.php
+++ models/Producto.php
@@ -3,11 +3,13 @@ class Producto {
     private $nombre;
     private $precio;
     private $descripcion;
+    private $imagen;
 
-    public function __construct($nombre, $precio, $descripcion){
+    public function __construct($nombre, $precio, $descripcion, $imagen){
         $this->nombre = $nombre;
         $this->precio = $precio;
         $this->descripcion = $descripcion;
+        $this->imagen = $imagen;
     }
 
     public function obtenerNombre(){
@@ -22,13 +24,17 @@ class Producto {
         return $this->descripcion;
     }
 
+    public function obtenerImagen(){
+        return "img/" . $this->imagen;
+    }
+
     // Método que simula obtener un producto de una base de datos
     public static function obtenerProductoId($id){
         // Simulación de datos de la base de datos
         $productos = [
-            1 => new Producto("Bicicleta electrica", 1900, "Bicicleta electrica de pedaleo asistido, con 3 velocidad y vateria de litio"),
-            2 => new Producto("Scooter electrico", 850, "Scooter electrico con autonomia de 50km"),
-            3 => new Producto("Bicicleta del montaña", 1100, "Bicicleta de montaña de 26\" con 18 velocidades y freno de disco")
+            1 => new Producto("Bicicleta electrica", 1900, "Bicicleta electrica de pedaleo asistido, con 3 velocidad y vateria de litio", "bici_electrica.jpg"),
+            2 => new Producto("Scooter electrico", 850, "Scooter electrico con autonomia de 50km","scooter.png"),
+            3 => new Producto("Bicicleta del montaña", 1100, "Bicicleta de montaña de 26\" con 18 velocidades y freno de disco","bici_26.png")
         ];
 
         return $productos[$id] ?? null; // Devolver producto por ID o null si no existe
--- views/producto.php
+++ views/producto.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detalle del Producto</title>
+    <link rel="stylesheet" href="css/estilos.css">
 </head>
 <body>
     <h1><?php echo $producto->obtenerNombre(); ?></h1>
-    <p>Descripcion: <?php echo $producto->obtenerDescripcion(); ?></p>
-    <p>Precio: $<?php echo $producto->obtenerPrecio(); ?></p>
+    <p><img src="<?php echo $producto->obtenerImagen(); ?>"> <strong>Descripcion:</strong> <?php echo $producto->obtenerDescripcion();?></p>
+    <p><strong>Precio:</strong> $<?php echo $producto->obtenerPrecio(); ?></p>
 </body>
 </html>
